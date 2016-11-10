f701a53468e4c5747f8056384474a2f9cf2b8b4d
Mohamed Moughamir <moughamir@gmail.com>
2016-11-10 14:25:46 +0000

    [+] FullScreen toggle

--- app/js/app.js
+++ app/js/app.js
@@ -12,8 +12,8 @@
 $(document).ready(function() {
 
     /*==========================================
-    =            Variables settings            =
-    ==========================================*/
+     =            Variables settings            =
+     ==========================================*/
     /**
 
      TODO:  - setting a variables array
@@ -24,15 +24,15 @@ $(document).ready(function() {
     var steps;
     steps = ["intro", "voyant", "pick", "draw", "interpret", "read", "resault"];
 
-    $.each(steps, function(i, step) {
+    $.each(steps, function (i, step) {
         var step = $("#" + step);
         console.log(step);
     });
 
     /*=====  End of Variables settings  ======*/
     $(".on").animate([
-        { transform: 'translate(0, 0, 0)' },
-        { transform: 'translate(0, -300px, 0)' }
+        {transform: 'translate(0, 0, 0)'},
+        {transform: 'translate(0, -300px, 0)'}
     ], {
         duration: 1000,
         iterations: Infinity
@@ -44,7 +44,7 @@ $(document).ready(function() {
      * TODO: Add Ambient sounce
      * */
     var sfxOne;
-    sfxOne = new Audio("../app/audio/ambient-forest-birds.m4a");
+    sfxOne = new Audio("app/audio/ambient-forest-birds.m4a");
     sfxOne.addEventListener('ended', function () {
         this.currentTime = 0;
         this.play();
@@ -52,9 +52,15 @@ $(document).ready(function() {
     sfxOne.play();
     console.log("Init Audio");
 
+    /*
+     * Let's go to full screen mode
+     * */
+
+    $(".fullscreen").click(function () {
+        $("#product-container").toggleFullScreen();
+        console.info("fullscrenn");
+    });
 });
-
-
 /* Cards fetcher*/
 
 //var app = angular.module('myApplication', ['ngSanitize'])
@@ -72,20 +78,17 @@ $(document).ready(function() {
 //    };
 //});
 
-var app = angular.module('myApplication', ['ngSanitize'])
-    .controller('postController', ['$scope', '$http', '$sce',
-        function postController($scope, $http, $sce) {
-
-            var url = './api.php/tarot';
-
-            $http.get(url).success(function(response) {
-                $scope.tarot = php_crud_api_transform(response).tarot
+var app;
+app = angular.module("myApplication", ["ngSanitize"])
+    .controller("postController", ["$scope", "$http", "$sce", function postController($scope, $http, $sce) {
+        var cards = "./api.php/tarot/";
+        $http.get(cards).success(function (response) {
+            $scope.tarot = php_crud_api_transform(response).tarot;
             });
             $scope.explicitlyTrustedHtml = $sce.trustAsHtml();
-            $scope.saySomething = function() {
-                    console.log('click click');
-                }
-        }
-    ]);
+        $scope.saySomething = function () {
+            console.log("Click Click");
+        };
+    }]);
 
 //
